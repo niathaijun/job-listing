c2bc8e93b5de6c5eb3a0c31716b910dd2e1e88f5
langweibo <niathaijun2016@gmail.com>
2017-04-19 22:09:50 +0800

    mistake

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :jobs
-  root 'jobs#index'
+    resources :jobs
+    root 'jobs#index'
 end
+
+  namespace :admin do
+    resources :jobs
+  end
